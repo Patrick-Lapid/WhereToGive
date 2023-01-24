d396ad9516206320aa3330fe9de030ced73ec438
sethMKennedy <62225478+sethMKennedy@users.noreply.github.com>
2023-01-24 13:38:44 -0500

    spacing

--- WHERETOGIVE-backend/main.go
+++ WHERETOGIVE-backend/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "github.com/Patrick-Lapid/WhereToGive/WHERETOGIVE-backend/routers"
+	"log"
+	"net/http"
+
+	"github.com/Patrick-Lapid/WhereToGive/WHERETOGIVE-backend/routers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    fmt.Println("Starting server on port 8000")
-    
-    router := mux.NewRouter()
+	fmt.Println("Starting server on port 8000")
 
-    routers.AddUserRouter(router)
-    
-    log.Fatal(http.ListenAndServe(":8000", router))
+	router := mux.NewRouter()
+
+	routers.AddUserRouter(router)
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
