f70af4e80afb3a6928856e759efcf9d8b308a6b3
sethMKennedy <62225478+sethMKennedy@users.noreply.github.com>
2023-01-30 16:37:30 -0500

    ITS WORKING ITS WORKING

--- WHERETOGIVE-backend/main.go
+++ WHERETOGIVE-backend/main.go
@@ -7,16 +7,23 @@ import (
 	"net/http"
 
 	"github.com/Patrick-Lapid/WhereToGive/WHERETOGIVE-backend/routers"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Starting server on port 8000")
 	fmt.Println("Attempting to connect to DB...")
+	/*cfg := mysql.Config{
+		User:   os.Getenv("root"),
+		Passwd: os.Getenv("letmein"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "where2give",
+	}*/
 	var err error
 	var db *sql.DB
-	db, err = sql.Open("mysql", "root:letmein@tcp(localhost:3306)/where2give")
+	db, err = sql.Open("mysql", "root:letmein@tcp(where2give)/sys")
 	if err != nil {
 		fmt.Println("Error validating sql.Open args")
 		log.Fatal(err)
@@ -33,12 +40,4 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 
-	/*cfg := mysql.Config{
-		User:   os.Getenv("root"),
-		Passwd: os.Getenv("letmein"),
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "where2give",
-	}*/
-	// Get a database handle.
 }
