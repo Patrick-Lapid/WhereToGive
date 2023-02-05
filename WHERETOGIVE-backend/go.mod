efdd29d521ee4804ce31b27e4b8223b23548e51d
DeepP2667 <deeppatel2667@gmail.com>
2023-02-04 21:59:33 -0500

    connect to hosted database, add gorm

--- WHERETOGIVE-backend/go.mod
+++ WHERETOGIVE-backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	gorm.io/driver/postgres v1.4.6
 	gorm.io/gorm v1.24.3
+	github.com/joho/godotenv v1.4.0
 )
 
 require (
--- WHERETOGIVE-backend/main.go
+++ WHERETOGIVE-backend/main.go
@@ -8,13 +8,16 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/Patrick-Lapid/WhereToGive/WHERETOGIVE-backend/routers"
+	"github.com/Patrick-Lapid/WhereToGive/WHERETOGIVE-backend/database"
 )
 
 func main() {
 	fmt.Println("Starting server on port 8000")
 
-	router := mux.NewRouter()
+	database.DB = database.InitDB()
 
+	router := mux.NewRouter()
+	
 	routers.AddUserRouter(router)
 
 	log.Fatal(http.ListenAndServe(":8000", router))
