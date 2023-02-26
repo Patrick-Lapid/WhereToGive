2553b3b5af21c35bdce3866a04728039729b59ab
Patrick-Lapid <patrickklapid@gmail.com>
2023-02-25 22:32:56 -0500

    added step feature

--- WHERETOGIVE-frontend/src/Landing.tsx
+++ WHERETOGIVE-frontend/src/Landing.tsx
@@ -1,6 +1,6 @@
-import { Text, Title, Button, createStyles } from '@mantine/core';
-import React from 'react';
-import { ChevronRight, Compass } from 'tabler-icons-react';
+import { Text, Title, Button, createStyles, Stepper, Group } from '@mantine/core';
+import React, { useEffect, useRef, useState } from 'react';
+import { ChevronRight, Compass, UserCheck, MapSearch, ShieldCheck } from 'tabler-icons-react';
 import Typewriter from 'typewriter-effect';
 import clipart from "../public/whereToGiveClipart.svg";
 import wave1 from "../public/wave1.svg";
@@ -8,10 +8,10 @@ import wave2 from "../public/wave2.svg";
 
 type Props = {}
 
-const useStyles = createStyles((theme) => ({
+const useStyles = createStyles((theme, _params, getRef) => ({
     HeroSection : {
         width: '70%',
-        height: '25rem',
+        height: '35rem',
         margin: '4rem auto',
         [theme.fn.smallerThan('lg')] : {
             width: '90%',
@@ -73,17 +73,84 @@ const useStyles = createStyles((theme) => ({
         
     },
 
+    infoBox : {
+        display : "flex",
+        flexDirection : "column",
+        alignItems : "center"
+    },
+
     infoTitle : {
         fontSize : "45px",
         margin: 'auto', 
-        textAlign : 'center'
+        textAlign : 'center',
+        [theme.fn.smallerThan('md')] : {
+            fontSize : "35px",
+        }
     },
 
-    infoBox : {
-        display : 'flex',
-        alignItems : 'center',
-        
+    stepSection : {
+        width: '70%',
+        margin : "auto",
     },
+
+    root: {
+        padding: theme.spacing.md,
+        width : "100%",
+        display : "flex",
+        flexDirection : "column",
+        marginTop : "3rem",
+        gap : "1rem",
+        alignItems : "center",
+        color : "white"
+      },
+    
+      separator: {
+        height: 2,
+        borderTop: `2px dashed ${theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4]}`,
+        borderRadius: theme.radius.xl,
+        backgroundColor: 'transparent',
+      },
+
+      stepDescription : {
+        color : 'rgb(246,246,246)',
+      },
+
+      content : {
+        fontSize : "20px",
+        textAlign : "center",
+        [theme.fn.smallerThan('md')] : {
+            margin : "2rem",
+        }
+
+      },
+    
+      separatorActive: {
+        borderWidth: 0,
+        backgroundColor: "white",
+      },
+    
+      stepIcon: {
+        ref: getRef('stepIcon'),
+        borderColor: 'transparent',
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.white,
+        borderWidth: 0,
+    
+        '&[data-completed]': {
+          borderWidth: 0,
+          backgroundColor: 'transparent',
+          backgroundImage: theme.fn.linearGradient(45, theme.colors.blue[8], theme.colors.cyan[2]),
+        },
+      },
+    
+      step: {
+        transition: 'transform 150ms ease',
+
+        color : "white",
+    
+        '&[data-progress]': {
+          transform: 'scale(1.05)',
+        },
+      },    
 
     footer : {
         backgroundColor : "#001220",
@@ -96,6 +163,23 @@ const useStyles = createStyles((theme) => ({
 const Landing = (props: Props) => {
 
     const {classes, cx}  = useStyles();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [active, setActive] = useState(1);
+    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
+    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
+    useEffect(() => {
+        // Check session storage for auth token
+        
+        let authToken = sessionStorage.getItem('Auth Token')
+        
+        if (authToken) {
+            setLoggedIn(true);
+            // generate user asynchronously
+            setActive(2);
+        }
+
+    }, []);
 
     return (
             <>
@@ -161,20 +245,43 @@ const Landing = (props: Props) => {
                 {/* InfoSection */}
                 <div className={classes.infoSection}>
                    {/* InfoSection Title */}
-                    <Title
-                        sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
-                        className={classes.infoTitle}
-                        fw={700}
-                        // color="dimmed"
-
-                    >
-                        Let us Navigate You 
-                        <Compass size={45} strokeWidth={1.5} style={{marginLeft: '10px'}}></Compass>
-                    </Title>
-                    
                     <div className={classes.infoBox}>
+                        <Title
+                            sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                            className={classes.infoTitle}
+                            fw={700}
+                            // color="dimmed"
+
+                        >
+                            Let us Navigate You 
+                            <Compass size={45} strokeWidth={1.5} style={{marginLeft: '10px'}}></Compass>
+                        </Title>
+                        
+                        {/* <div className={classes.stepSection}> */}
+                            <Stepper iconSize={65} size={'xl'} active={active} classNames={classes} onStepClick={setActive} breakpoint="md">
+                                <Stepper.Step label="First" description="Create Account" icon={<UserCheck size={31} />}>
+                                Create an account / Login to Existing account
+                                </Stepper.Step>
+                                <Stepper.Step label="Second" description="Browse Charities" icon={<MapSearch size={31} />}>
+                                Complete Survey to find charities tailored to you / Browse Charity Dashboard
+                                </Stepper.Step>
+                                <Stepper.Step label="Full Access" description="Track Donations" icon={<ShieldCheck size={31} />}>
+                                Self Report Donations to Charities / Save Favorite Charities for Later
+                                </Stepper.Step>
+                            </Stepper>
+
+                            {/* <Group position="center" mt="xl">
+                                <Button variant="default" onClick={prevStep}>Back</Button>
+                                <Button onClick={nextStep}>Next step</Button>
+                            </Group> */}
+
+                        {/* </div> */}
 
                     </div>
+                    
+                    
+                  
+
                 </div>
 
                 {/* Wave2 spacer */}
