242e73c8d5cbb35ebfd618a4ccb89a364732f2d6
DeepP2667 <70382249+DeepP2667@users.noreply.github.com>
2023-02-28 18:24:26 -0500

    Create and Delete user test

--- WHERETOGIVE-backend/tests/user_test.go
+++ WHERETOGIVE-backend/tests/user_test.go
@@ -1,9 +1,9 @@
 package tests
 
 import (
+	"bytes"
 	"encoding/json"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
 	"testing"
 
@@ -27,6 +27,7 @@ func TestGetUser(t *testing.T) {
 	assert.NotEqual(t, user.City, "", "City not found")
 	assert.NotEqual(t, user.State, "", "State not found")
 	assert.NotEqual(t, user.Userid, 0, "Userid not found")
+	resp.Body.Close()
 }
 
 func TestGetUserNotFound(t *testing.T) {
@@ -34,14 +35,16 @@ func TestGetUserNotFound(t *testing.T) {
 	if errGetting != nil {
         t.Fatalf("Couldn't make request: %v\n", errGetting)
     }
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
     if err != nil {
-        log.Fatal(err)
+        t.Fatalf("Couldn't read request: %v\n", err)
     }
+
     responseString := string(body)
 
 	assert.Equal(t, resp.StatusCode, http.StatusNotFound, "The status of this request should be 404 (Not Found)")
 	assert.Equal(t, responseString, "User not found", "User should not be found if not in database")
+	resp.Body.Close()
 }
 
 func TestGetUserInvalidParameterType(t *testing.T) {
@@ -49,12 +52,69 @@ func TestGetUserInvalidParameterType(t *testing.T) {
 	if errGetting != nil {
         t.Fatalf("Couldn't make request: %v\n", errGetting)
     }
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
     if err != nil {
-        log.Fatal(err)
+        t.Fatalf("Couldn't read request: %v\n", err)
     }
+
     responseString := string(body)
 
 	assert.Equal(t, resp.StatusCode, http.StatusBadRequest, "The status of this request should be 400 (Bad Request)")
 	assert.Equal(t, responseString, "userID must be an integer", "Response for string type parameter should be (userID must be an integer)")
+	resp.Body.Close()
+}
+
+func TestCreateUser(t *testing.T) {
+	user := &models.User{
+		Userid: 9999,
+		FirstName: "TestFirst",
+		LastName: "TestLast",
+		City: "G",
+		State: "FL",
+	}
+	
+	postBody := new(bytes.Buffer)
+	json.NewEncoder(postBody).Encode(user)
+
+	resp, errGetting := http.Post("http://localhost:8000/api/users", "application/json", postBody)
+	if errGetting != nil {
+        t.Fatalf("Couldn't make request: %v\n", errGetting)
+    }
+
+	body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        t.Fatalf("Couldn't read request: %v\n", err)
+    }
+
+    responseString := string(body)
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "The status of this request should be 200 (OK)")
+	assert.Equal(t, responseString, "Successfully created user", "Response for creation should be (Successfully created user)")
+	resp.Body.Close()
+}
+
+func TestDeleteUser(t *testing.T) {
+	client := &http.Client{}
+
+    request, errCreate := http.NewRequest("DELETE", "http://localhost:8000/api/users/9999", nil)
+    if errCreate != nil {
+        t.Fatalf("Couldn't create delete request: %v\n", errCreate)
+        return
+    }
+
+    resp, errReq := client.Do(request)
+    if errReq != nil {
+        t.Fatalf("Couldn't make delete request: %v\n", errReq)
+    }
+
+	body, errRead := io.ReadAll(resp.Body)
+    if errRead != nil {
+        t.Fatalf("Couldn't read request: %v\n", errRead)
+    }
+
+	responseString := string(body)
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "The status of this request should be 200 (OK)")
+	assert.Equal(t, responseString, "Successfully deleted user", "Response for creation should be (Successfully deleted user)")
+	resp.Body.Close()
 }
