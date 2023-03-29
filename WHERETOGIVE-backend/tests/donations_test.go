c6b2c8eb6f9b4c87484425752b21bd1e255324bf
DeepP2667 <70382249+DeepP2667@users.noreply.github.com>
2023-03-29 14:55:22 -0400

    donations tests

---
+++ WHERETOGIVE-backend/tests/donations_test.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/Patrick-Lapid/WhereToGive/WHERETOGIVE-backend/models"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateDonation(t *testing.T) {
+	donation := &models.Donation{
+		Userid: 2,
+		Charityid: 11,
+		Amount: 10,
+	}
+	
+	postBody := new(bytes.Buffer)
+	json.NewEncoder(postBody).Encode(donation)
+
+	resp, errGetting := http.Post("http://localhost:8000/api/donations/add", "application/json", postBody)
+	if errGetting != nil {
+        t.Fatalf("Couldn't make request: %v\n", errGetting)
+    }
+
+	body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        t.Fatalf("Couldn't read request: %v\n", err)
+    }
+
+    responseString := string(body)
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "The status of this request should be 200 (OK)")
+	assert.Equal(t, responseString, "Successfully created donation", "Response for creation should be (Successfully created user)")
+	resp.Body.Close()
+}
+
+func TestGetDonationsByUser(t *testing.T) {
+	resp, errGetting := http.Get("http://localhost:8000/api/donations/getdonations/2")
+	if errGetting != nil {
+        t.Fatalf("Couldn't make request: %v\n", errGetting)
+    }
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "The status of this request should be 200 (OK)")
+
+	var donation []models.Donation;
+	json.NewDecoder(resp.Body).Decode(&donation)
+	assert.NotEqual(t, donation[0].Userid, "", "Userid not found")
+	assert.NotEqual(t, donation[0].Charityid, "", "Charityid not found")
+	assert.NotEqual(t, donation[0].Amount, 0, "Amount is 0")
+	resp.Body.Close()
+}
+
+
+func TestGetUserTotalAmount(t *testing.T) {
+	resp, errGetting := http.Get("http://localhost:8000/api/donations/getamount/2")
+	if errGetting != nil {
+        t.Fatalf("Couldn't make request: %v\n", errGetting)
+    }
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK, "The status of this request should be 200 (OK)")
+
+	var total models.TotalAmount;
+	json.NewDecoder(resp.Body).Decode(&total)
+	assert.NotEqual(t, total.TotalAmount, 0, "Total donation amount is 0")
+	resp.Body.Close()
+}
