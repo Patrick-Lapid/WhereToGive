e24da0eddaa1044a94448287572ef585cd59bdb5
Ai Linh Nguyen <anguyen4@wpi.edu>
2023-04-19 14:04:38 -0400

    removed uneccesary comments

--- WHERETOGIVE-frontend/cypress/e2e/CharitySearch-spec.cy.ts
+++ WHERETOGIVE-frontend/cypress/e2e/CharitySearch-spec.cy.ts
@@ -19,20 +19,18 @@ describe('Charity Search component', () => {
   });
 
   it('should render the carousel when charities are loaded', () => {
-    // Replace this with the actual action that loads the charities, such as selecting tags and clicking the search button
-    // For example:
+
     cy.get('[data-cy=multi-select-input]', { timeout: 10000 }).type('adoption{enter}');
 
     cy.get('button').contains('Find the perfect charities for you').click();
 
     // Check if the carousel is rendered
     cy.get('[data-cy=carousel]').should('exist');
-    // cy.get('div[role="listbox"]').should('exist');
+   
   });
 
   it('should render the CharityTiles component when charities are loaded', () => {
-    // Replace this with the actual action that loads the charities, such as selecting tags and clicking the search button
-    // For example:
+  
     cy.get('[data-cy=multi-select-input]').type('animals{enter}');
     cy.get('button').contains('Find the perfect charities for you').click();
   });
